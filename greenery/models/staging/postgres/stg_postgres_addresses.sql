917f9469b0ca40b5d3810d1025da0060fa111a1b
vikasmech <vikasmech.nitk@gmail.com>
2023-04-16 20:57:39 +0000

    first changes

---
+++ greenery/models/staging/postgres/stg_postgres_addresses.sql
@@ -0,0 +1,13 @@
+with source as (
+
+    select * from {{ source ('postgres', 'addresses') }}
+), renamed_recast as (
+    SELECT
+        address_id as address_guid
+        , address
+        , state
+        , lpad(zipcode, 5, 0) as zip_code  
+        , country 
+    from source 
+)
+select * from renamed_recast
